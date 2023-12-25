656f941f0b4781e07204bd9b5c4e8f3a53ddea73
Jedai <142099709+yama4a@users.noreply.github.com>
2023-12-25 06:00:53 +0300

    Add files via upload

---
+++ modules/__init__.py
@@ -0,0 +1,6 @@
+from .exchange_withdraw import ExchangeWithdraw, OkxWithdraw
+from .transfer import Transfer
+from .debank import DeBank
+from .tx_checker import TxChecker
+from .balance_checker import EvmBalanceChecker, StarknetBalanceChecker
+from .nft_checker import NFTChecker
---
+++ modules/utils/titles.py
@@ -0,0 +1,37 @@
+import random
+
+title_1 = '''
+'''
+
+title_2 = '''
+'''
+
+title_3 = '''
+'''
+
+title_4 = '''
+'''
+
+title_5 = '''
+'''
+
+title_6 = '''
+'''
+
+title_7 = '''
+'''
+
+
+titles = [
+    title_1, 
+    title_2, 
+    title_3, 
+    title_4,
+    title_5,
+    title_6,
+    title_7,
+    ]
+colors = ['green', 'yellow', 'blue', 'magenta', 'cyan']
+
+TITLE       = random.choice(titles)
+TITLE_COLOR = random.choice(colors)
